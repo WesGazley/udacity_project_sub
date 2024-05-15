a0fb1791c199f6af714e08319e9e207307aa9fac
Westley-Gazley <lyghtpass@gmail.com>
2024-05-15 19:29:45 -0400

    Added SQL file and added excel data to gitignore

---
+++ SQL/data.sql
@@ -0,0 +1,22 @@
+-- Question 1 We want to understand more about the movies that families are watching.
+
+SELECT 
+    f.title,
+    c.name,
+    COUNT(*) AS rental_count
+FROM 
+    category c
+JOIN 
+    film_category fc ON c.category_id = fc.category_id
+JOIN 
+    film f ON f.film_id = fc.film_id
+JOIN 
+    inventory i ON f.film_id = i.film_id
+JOIN 
+    rental r ON i.inventory_id = r.inventory_id
+WHERE 
+    c.name IN ('Animation', 'Children', 'Classics', 'Comedy', 'Family', 'Music')
+GROUP BY 
+    f.title, c.name
+ORDER BY 
+    c.name, f.title;
