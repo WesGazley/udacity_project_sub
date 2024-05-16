5c29b0e4bbf2a619b96ed6b800fb76b771026bcd
Westley-Gazley <lyghtpass@gmail.com>
2024-05-15 20:07:44 -0400

    Reformated SQL code

--- SQL/data.sql
+++ SQL/data.sql
@@ -1,6 +1,6 @@
 -- Question 1 We want to understand more about the movies that families are watching.
 
-SELECT 
+ /* SELECT 
     f.title,
     c.name,
     COUNT(*) AS rental_count
@@ -19,4 +19,23 @@ WHERE
 GROUP BY 
     f.title, c.name
 ORDER BY 
-    c.name, f.title;
+    c.name, f.title; */
+
+    SELECT f.title,
+       c.NAME,
+       Count(*) AS rental_count
+FROM   category c
+       JOIN film_category fc
+         ON c.category_id = fc.category_id
+       JOIN film f
+         ON f.film_id = fc.film_id
+       JOIN inventory i
+         ON f.film_id = i.film_id
+       JOIN rental r
+         ON i.inventory_id = r.inventory_id
+WHERE  c.NAME IN ( 'Animation', 'Children', 'Classics', 'Comedy',
+                   'Family', 'Music' )
+GROUP  BY f.title,
+          c.NAME
+ORDER  BY c.NAME,
+          f.title;
